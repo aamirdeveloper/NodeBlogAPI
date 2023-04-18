d11b347af7d34d30d3c1c1a8ab59876dfc6cd4f8
Nodejs <aamir.0019@gmail.com>
2023-04-18 19:33:47 +0530

    final

---
+++ connection.js
@@ -0,0 +1,16 @@
+let mysql = require('mysql');
+
+let connection = mysql.createConnection({
+    host: '156.67.222.148',
+    user: 'u498357097_nodeAPI',
+    password: 'X;5lu068!nK',
+    database: 'u498357097_nodeAPI'
+});
+
+connection.connect(function(err) {
+    if (err) {
+      return console.error('error: ' + err.message);
+    }
+  
+    console.log('Connected to the MySQL server.');
+  });
