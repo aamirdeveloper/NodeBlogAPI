a700961b71b4e23ed372659aaf7353f9065d26e9
Nodejs <aamir.0019@gmail.com>
2023-04-19 19:27:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const cors = require("cors");
 
 const app = express();
 
@@ -11,6 +12,7 @@ const postsRoute = require('./routes/posts');
 const userRoute = require('./routes/user');
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/posts", postsRoute);
 app.use("/user", userRoute);
