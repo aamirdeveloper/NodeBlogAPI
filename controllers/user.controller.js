bb544488a23a43aee3c0d89719ad9296514cd67b
Nodejs <aamir.0019@gmail.com>
2023-04-21 17:03:56 +0530

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -42,7 +42,7 @@ function signUp(req, res){
 function login(req, res){
     models.User.findOne({where:{email:req.body.email}}).then(result =>{
         if(result === null){
-            res.status(405).json({
+            res.status(409 Conflict).json({
                 message: "Invalid Credentials"
             });
         }else{
@@ -58,7 +58,7 @@ function login(req, res){
                         });
                     });
                 }else{
-                    res.status(405).json({
+                    res.status(409 Conflict).json({
                         message: "Invalid Credentials"
                     });
                 }
