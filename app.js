dfdfe67357fcb8bea69761f591bc30917c82ee43
Nodejs <aamir.0019@gmail.com>
2023-04-25 11:55:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,11 +14,13 @@ app.use(express.urlencoded({ extended: false }));
 
 const postsRoute = require('./routes/posts');
 const userRoute = require('./routes/user');
+const categoryRoute = require('./routes/category');
 
 app.use(bodyParser.json());
 
 
 app.use("/posts", postsRoute);
 app.use("/user", userRoute);
+app.use("/category", categoryRoute);
 
 module.exports = app;
